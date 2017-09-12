057e588a7834844b56c82c1254da85fac4adc5a5
Jennifer Solman <jennifer.solman@puppet.com>
2017-09-12 15:02:06 -0700

    currently refactoring and working on not resting while being attacked by archer

--- player.rb
+++ player.rb
@@ -1,22 +1,37 @@
 class Player
 
-
   def play_turn(warrior)
-    
+     
+    #The warrior does not need rest
     if(!needRest?(warrior))
+      strongWarrior(warrior)
+
+    #The warrior needs rest
+    else
+      warriorNeedsRest(warrior)
+    end
+  end
+  def warriorNeedsRest(warrior)
+      #If something is directly in front of her
+      if(!coastClear?(warrior))
+        warrior.walk!(:backward)
+      #If nothing is directly in front of her  
+      elsif(coastClear?(warrior))
+        warrior.rest!
+      else
+        warrior.attack!
+      end 
+  end
+
+
+  def strongWarrior(warrior)
+      #Walk if no one is in from of her
       if(warrior.feel.empty?)
         warrior.walk!
+      #Attack if something is in front of her
       else
         warrior.attack!
       end  
-    elsif(!coastClear?(warrior))
-        warrior.walk!(:backward)
-    elsif(coastClear?(warrior))
-      warrior.rest!
-    else
-      warrior.attack!
-    end 
-    
   end
 
   def needRest?(warrior)
@@ -34,4 +49,6 @@ class Player
       false
     end
   end
+
 end
+
