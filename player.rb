d5f9eb3e1d7df4dbf275cbb7f633ac7221845f45
Jennifer Solman <jennifer.solman@puppet.com>
2017-09-12 15:45:24 -0700

    defeated archer. Next up: unset archer flag after archer dies

--- player.rb
+++ player.rb
@@ -1,41 +1,68 @@
 class Player
+  
+  :backward
+  :attack
+  :forward
+  :rest
+  
+  def initialize()
+     @minHealth = 20
+     @lastMove = 'nothing'
+     @lastHealth = 0
+     @archerAttack = false
+  end
 
   def play_turn(warrior)
-     
-    #The warrior does not need rest
-    if(!needRest?(warrior))
-      strongWarrior(warrior)
+    print "Archer Attack? "
+    puts @archerAttack
 
+    if(archerAttack?(warrior))
+      @archerAttack = true
+    end    
+    #warrior is strong or is being attacked by an archer
+    if(!needRest?(warrior) || @archerAttack)
+      beAggressive(warrior)
     #The warrior needs rest
     else
-      warriorNeedsRest(warrior)
+      rest(warrior)
     end
+    @lastHealth = warrior.health
   end
-  def warriorNeedsRest(warrior)
+
+  def rest(warrior)
       #If something is directly in front of her
       if(!coastClear?(warrior))
         warrior.walk!(:backward)
+        @lastMove = :backward
       #If nothing is directly in front of her  
       elsif(coastClear?(warrior))
         warrior.rest!
-      else
-        warrior.attack!
+        @lastMove = :rest
       end 
   end
 
+  def archerAttack?(warrior)
+    if( @lastMove == :rest && warrior.health < @lastHealth )
+      true
+    else
+      false
+    end
+  end  
 
-  def strongWarrior(warrior)
+  def beAggressive(warrior)
       #Walk if no one is in from of her
       if(warrior.feel.empty?)
         warrior.walk!
+        @lastMove = :forward
       #Attack if something is in front of her
       else
         warrior.attack!
+        @lastMove = :attack
       end  
   end
 
   def needRest?(warrior)
-    if(warrior.health > 10)
+    if(warrior.health >= @minHealth)
       false
     else 
       true
@@ -49,6 +76,5 @@ class Player
       false
     end
   end
-
 end
 
